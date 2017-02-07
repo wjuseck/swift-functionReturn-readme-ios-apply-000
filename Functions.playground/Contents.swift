3b1bbea65f4887807efc7ae58ac423f02dae7f81
wjuseck <will.juseck@gmail.com>
2017-02-07 11:19:28 -0500

    Made more age functions

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -1,17 +1,19 @@
+
+
 func printAge(name: String) {
     print("\(name) is 29")
 }
 
-printAge("Adam")
-
+printAge(name: "Adam")
 
 func happyBirthday(age: Int) {
     print("Happy birthday #\(age)!")
 }
 
+happyBirthday(age: 28)
 
 func getAge(name: String) -> Int {
-    return 29
+    return 34
 }
 
 // Uncomment to see the error
@@ -21,17 +23,24 @@ func badGetAge(name: String) -> Int {
 }
 */
 
+let friend = "John"
+var friendAge = getAge(name: friend)
+happyBirthday(age: friendAge)
 
-let friend = "Emily"
-var friendAge = getAge(friend)
-happyBirthday(friendAge)
+
+func birthdayTester(oldness: Int) -> String {
+    return "Happy \(oldness), John!"
+}
+
+birthdayTester(oldness: 34)
+
 
 
 func birthdayGreeting(age: Int) -> String {
     return "Happy birthday #\(age)!"
 }
 
-var greeting = birthdayGreeting(30)
+var greeting = birthdayGreeting(age: 30)
 print(greeting)
 
 
@@ -42,4 +51,4 @@ func getAgeAndCongratulate(name: String) -> Int {
 }
 
 let friend2 = "Pete"
-var friend2Age = getAgeAndCongratulate(friend2)
+var friend2Age = getAgeAndCongratulate(name: friend2)
